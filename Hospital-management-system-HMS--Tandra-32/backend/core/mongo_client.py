2d991d08d5bc34296af6024d0db77127f3f9efbc
Md Monowarul Aziz <azizmonowarul61@gmail.com>
2026-05-10 23:57:56 +0600

    Delete Hospital-management-system-HMS--Tandra-32 directory

--- Hospital-management-system-HMS--Tandra-32/backend/core/mongo_client.py
+++
@@ -1,8 +0,0 @@
-import os
-from pymongo import MongoClient
-
-MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017')
-client = MongoClient(MONGO_URI)
-
-def get_database():
-    return client['hospital_db']
